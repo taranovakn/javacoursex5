2c5276b8457d36f2ff90d02950dd869f3dace1a9
galyamovakn <galyamovakn@gmail.com>
2020-09-09 13:30:03 +0300

    Домашнее задание 2. Правки по ревью

--- src/ru/x5javacourse/Main.java
+++ src/ru/x5javacourse/Main.java
@@ -8,26 +8,46 @@ import static ru.x5javacourse.hw2.HW2.*;
 public class Main {
     public static void main(String [] args) throws IOException
     {
-        System.out.println("Какое домашнее задание хотите проверить? Введите с клавиатуры 1, 2 или 3");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        boolean continueAnswer = true;
 
-        int hw = readIntFromKeyboard();
+        while (continueAnswer){
+            System.out.println("Какое домашнее задание хотите проверить? Введите с клавиатуры 1, 2 или 3");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        if (hw == 1)
-        {
-            System.out.println("Hello world!");
-        }
-        else
-            if (hw == 2)
-            {
-                hw2_choosing();
-            }
-            else
-                if (hw == 3)
-                {
+            int hw = readIntFromKeyboard();
+
+            switch (hw) {
+                case 1: // 1. Ввести целое число в консоли и вывести на экран последнюю цифру введенного числа.
+                    System.out.println("Hello world!");
+                    break;
+                case 2:
+                    hw2Choosing();
+                    break;
+                case 3:
                     System.out.println("Здесь будет ДЗ-3");
-                }
-                else System.out.println("Перезапустите программу и выберите корректный номер домашнее задания на проверку");
-        System.out.println("Конец");
+                    break;
+            }
+            continueAnswer = continueProgram();
+        }
+        System.out.println("Завершение программы.");
+    }
+
+    public static Boolean continueProgram() throws IOException {
+        System.out.println("Хотите продолжить и проверить другое задание? Y - продолжить, N - завершить программу");
+        boolean continueProgram = false;
+        boolean isNotCorrectInput = true;
+        while (isNotCorrectInput) {
+                String continueAnswer = readerFromKeyboard();
+                if (continueAnswer.equals("Y")){
+                    continueProgram = true;
+                    isNotCorrectInput = false;
+                } else
+                    if (continueAnswer.equals("N")){
+                        continueProgram = false;
+                        isNotCorrectInput = false;
+                    }
+                    else System.out.println("Введите Y или N.");;
+        }
+        return continueProgram;
     }
 }
--- src/ru/x5javacourse/hw2/HW2.java
+++ src/ru/x5javacourse/hw2/HW2.java
@@ -5,7 +5,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class HW2 {
-    public static <string> void hw2_choosing() throws IOException {
+    public static <string> void hw2Choosing() throws IOException {
 
         System.out.println("Выберите какое из вычиcлений вы хотите проверить");
         System.out.println("1. Ввести целое число в консоли и вывести на экран последнюю цифру введенного числа.");
@@ -18,11 +18,11 @@ public class HW2 {
         System.out.println("8. Для массива найти и вывести на экран: максимальное значение; сумму положительных элементов; сумму четных отрицательных элементов; количество положительных элементов; среднее арифметическое отрицательных элементов.");
         System.out.println("9. Переставить элементы массива в обратном порядке. Вывести результат в консоль.");
         System.out.println("10. Переместить нули в конец массива. Вывести результат в консоль. ");
-        Integer number_of_calculation = readIntFromKeyboard();
+        Integer numberOfCalculation = readIntFromKeyboard();
 
-        switch (number_of_calculation) {
+        switch (numberOfCalculation) {
             case 1: // 1. Ввести целое число в консоли и вывести на экран последнюю цифру введенного числа.
-                theLastSymbolOfNumber();
+                calculateTheLastSymbolOfNumber();
                 break;
             case 2: // 2. Ввести целое трехзначное число в консоли и найти сумму цифр этого трехзначного числа.
                 sumOfNumbers();
@@ -40,7 +40,7 @@ public class HW2 {
                 sumOfNumbersWithCalculationCase4();
                 break;
             case 5: // 5. Ввести три целых числа с консоли. Вывести на экран наименьшее из них.
-                compare3SymbolsNumbers();
+                compareNumbers();
                 break;
             case 6: // 6. Ввести целое число в консоли. Вывести его строку-описание вида «отрицательное четное число»,
                 // «нулевое число»,  «положительное нечетное число» и т. д.
@@ -49,7 +49,7 @@ public class HW2 {
             case 7:
                 // 7. Написать программу, вычисляющую стоимость 10 минутного междугороднего разговора
                 // в зависимости от кода города.
-                CallPrice();
+                callPrice();
                 break;
             case 8:
                 // 8. Для массива найти и вывести на экран: максимальное значение; сумму положительных элементов;
@@ -67,7 +67,7 @@ public class HW2 {
                 revertOfArray(numArrayForCase9);
                 break;
             case 10: // 10. Переместить нули в конец массива. Вывести результат в консоль.
-                int[] numArrayForCase10 = {15,10,0,-6,-5,3,0,-34,0,32,56,0,24,-52};
+                int[] numArrayForCase10 = {15, 10, 0, -6, -5, 3, 0, -34, 0, 32, 56, 0, 24, -52};
                 moveZeroToTheEndOfArray(numArrayForCase10);
                 break;
 
@@ -75,64 +75,74 @@ public class HW2 {
         //else System.out.println("К сожалению вы ввели некорректный номер, перезапустите приложение и выберите снова.
         // Пока только так:( Но я учусь:) ");
     }
+
     public static void descriptionOfNumber() throws IOException {
         System.out.println("Введите целое число");
-        Integer number_for_case6 = readIntFromKeyboard();
-        boolean positive_flag = positive_number(number_for_case6);
-        boolean odd_flag = odd_number(number_for_case6); // нечетное
-        if (number_for_case6 == 0) {
-            System.out.println(number_for_case6 + " - нулевое число");
-        } else if (odd_flag) {
-            if (positive_flag) {
-                System.out.println(number_for_case6 + " - это нечетное положительное число");
-            } else System.out.println(number_for_case6 + " - это нечетное отрицательное  число");
-        } else if (positive_flag) {
-            System.out.println(number_for_case6 + " - это четное положительное число");
-        } else System.out.println(number_for_case6 + " - это четное отрицательное  число");
+        Integer number = readIntFromKeyboard();
+        String positiveOrNegativeDefinition = positiveOrNegativeNumberStringDescription(number);
+        String oddOrEvenDefinition = evenOrOddNumberStringDescription(number);
+
+       if (number == 0) {
+           System.out.println(number + " - нулевое число");
+        } else System.out.println(number + " - это " +positiveOrNegativeDefinition + oddOrEvenDefinition+ "число");
     }
-    public static void compare3SymbolsNumbers() throws IOException {
+
+    public static void compareNumbers() throws IOException {
+
         int[] numbers;
-        numbers = new int[3];
-        for (int i = 1; i <= 3; i++) {
+        int countOfNumbersToCompare = correctCountInput();
+
+        numbers = new int[countOfNumbersToCompare];
+        for (int i = 1; i <= countOfNumbersToCompare; i++) {
             System.out.println("Введите " + i + "-е целое число");
             numbers[i - 1] = readIntFromKeyboard();
         }
-        int biggest_number = maximumNumber(numbers);
-        System.out.println("Наибольшее число " + biggest_number);
+        int biggestNumber;
+        biggestNumber = maximumNumber(numbers);
+        System.out.println("Наибольшее число " + biggestNumber);
+    }
+    public static int correctCountInput() throws IOException {
+        System.out.println("Введите количество числе для сравнения.");
+        boolean isNotCorrectInput = true;
+        while (isNotCorrectInput) {
+        int countOfComparableNumbers = readIntFromKeyboard();
+        if (countOfComparableNumbers >0){
+            return countOfComparableNumbers;
+        } else System.out.println("Вы ввели некорректное количество.");
+    }
+        return 0;
     }
     public static void sumOfNumbersWithCalculationCase4 () throws IOException {
         System.out.println("Введите число (Возможен ввод положительных и отрицательных чисел)");
-        int number_for_case4 = readIntFromKeyboard();
-        if (number_for_case4 > 0) {
-            number_for_case4++;
+        int number = readIntFromKeyboard();
+        if (number > 0) {
+            number++;
         } else
-            if (number_for_case4 < 0) {
-            number_for_case4 += 2;
-            } else
-                if (number_for_case4 == 0) {
-                    number_for_case4 = 10;
-                }
-        System.out.println("Итоговое число: " + number_for_case4);
+            if (number < 0) {
+                number += 2;
+            } else number = 10;
+
+        System.out.println("Итоговое число: " + number);
     }
     public static void sumOfNumbersWithCalculation() throws IOException {
         System.out.println("Введите число (Возможен ввод положительных и отрицательных чисел)");
-        Integer number_for_case3 = readIntFromKeyboard();
-        if (number_for_case3 > 0) {
-            number_for_case3++;
+        Integer sumOfNumbersWithCalculation = readIntFromKeyboard();
+        if (sumOfNumbersWithCalculation > 0) {
+            sumOfNumbersWithCalculation++;
         }
-        System.out.println("Итоговое число: " + number_for_case3);
+        System.out.println("Итоговое число: " + sumOfNumbersWithCalculation);
     }
     public static void sumOfNumbers() throws IOException {
         System.out.println("Введите трехзначное число");
-        int number_for_sum = readIntFromKeyboard();
-        int sum = number_for_sum / 100 + number_for_sum / 10 % 10 + number_for_sum % 10;
-        System.out.println("Сумма цифр трехначного числа " + sum);
+        int numberForSum = readIntFromKeyboard();
+        int sumOfNumbers = numberForSum / 100 + numberForSum / 10 % 10 + numberForSum % 10;
+        System.out.println("Сумма цифр трехначного числа " + sumOfNumbers);
     }
-    public static void theLastSymbolOfNumber() throws IOException {
+    public static void calculateTheLastSymbolOfNumber() throws IOException {
         System.out.println("Введите число");
-        int number_for_the_last_symbol = readIntFromKeyboard();
-        number_for_the_last_symbol = number_for_the_last_symbol % 10;
-        System.out.println("Последняя цифра введенного числа " + number_for_the_last_symbol);
+        int calculateTheLastSymbolOfNumber = readIntFromKeyboard();
+        calculateTheLastSymbolOfNumber = calculateTheLastSymbolOfNumber % 10;
+        System.out.println("Последняя цифра введенного числа " + calculateTheLastSymbolOfNumber);
     }
     public static void revertOfArray(int[] numArray){
 
@@ -166,7 +176,6 @@ public class HW2 {
         int maxNumber = maximumNumber(numArray);
         System.out.println("Максимальное число в массиве " + maxNumber);
     }
-
     public static void sumOfPositiveElements(int[] numArray) {
         int sumOfPositiveElements = 0;
         for (int i = 0; i < numArray.length; i++) {
@@ -175,34 +184,31 @@ public class HW2 {
         }
         System.out.println("Сумма положительных элементов в массиве " + sumOfPositiveElements);
     }
-
     public static void sumOfOddNegativeNumbers(int[] numArray) {
         int sumOfOddNegativeNumbers = 0;
         for (int i = 0; i < numArray.length; i++) {
 
-            if (negative_number(numArray[i]) && even_number(numArray[i])) {
+            if (negativeNumber(numArray[i]) && isEvenNumber(numArray[i])) {
                 sumOfOddNegativeNumbers = sumOfOddNegativeNumbers + numArray[i];
             }
         }
         System.out.println("Сумма четных отрицательных элементов в массиве " + sumOfOddNegativeNumbers);
     }
-
     public static void countOfPositiveNumbers(int[] numArray){
         int countOfPositiveNumbers = 0;
         for (int i = 0; i < numArray.length; i++) {
 
-            if (positive_number(numArray[i]) )
+            if (positiveNumber(numArray[i]) )
                 countOfPositiveNumbers ++;
         }
         System.out.println("Количество положительных элементов в массиве (не включая 0) " + countOfPositiveNumbers);
     }
-
     public static void averageOfNegativeNumbers(int[] numArray){
         double counterOfNegativeNumbers;
         counterOfNegativeNumbers = 0;
         double sumOfNegativeNumbers = 0;
         for (int i = 0; i < numArray.length; i++) {
-            if (negative_number(numArray[i])) {
+            if (negativeNumber(numArray[i])) {
                 sumOfNegativeNumbers = sumOfNegativeNumbers + numArray[i];
                 counterOfNegativeNumbers++;
             }
@@ -210,86 +216,119 @@ public class HW2 {
         double averageOfNegativeNumbers = sumOfNegativeNumbers / counterOfNegativeNumbers;
         System.out.println("Среднее арифметическое отрицательных чисел в массиве: " + averageOfNegativeNumbers);
     }
-
     public static Integer maximumNumber (int[] arrayOfNumbers)  {
         int maxNumber = arrayOfNumbers[0];
         for (int n = 0; n < arrayOfNumbers.length; n++) {
             if  (maxNumber <= arrayOfNumbers[n]) {
                 maxNumber = arrayOfNumbers[n];
             }
-            else;
         }
         return maxNumber;
     }
+    public static void callPrice() throws IOException {
 
-    public static void CallPrice() throws IOException {
         System.out.println("Введите код города: Москва(905), Ростов(194), Краснодар(491), Киров(800)");
-        Integer codeForCase7 = readIntFromKeyboard();
+        int codeOfCity = readIntFromKeyboard();
         System.out.println("Сколько минут планируется звонок?");
-        Integer minForCall = readIntFromKeyboard();
-        double finalCallPrice = 0;
-        if (codeForCase7 == 905) {
-            finalCallPrice = minForCall * 4.15;
-            System.out.println("Москва. Стоимость разговора за "+minForCall + " минут:" + finalCallPrice +"руб");
-        } else
-            if (codeForCase7 == 194) {
-                finalCallPrice = minForCall * 1.98;
-                System.out.println("Ростов. Стоимость разговора за "+minForCall + " минут:" + finalCallPrice +"руб");
-        } else
-            if (codeForCase7 == 491) {
-                finalCallPrice = minForCall * 2.69;
-                System.out.println("Краснодар. Стоимость разговора за "+minForCall + " минут:" + finalCallPrice +"руб");
-            } else
-            if (codeForCase7 == 800) {
-                finalCallPrice = minForCall * 5;
-                System.out.println("Киров. Стоимость разговора за "+minForCall + "минут:" + finalCallPrice +"руб");
-            } else System.out.println("Вы ввели некорректный код города, перезапустите программу и попробуйте еще раз");
+        int minForCall = readIntFromKeyboard();
+        System.out.println(cityNames(codeOfCity) + "Стоимость разговора за "+minForCall + " минут:"
+                + priceForCall(codeOfCity,minForCall) +"руб");
     }
-    public static String reader_from_keyboard() throws IOException {
+    public static String cityNames(int codeOfCity){
+        String cityNames;
+        switch (codeOfCity){
+            case 905: 
+                cityNames = "Москва(905) ";
+                break;
+            case 194: 
+                cityNames = "Ростов (194) ";
+                break;
+            case 491:
+                cityNames = "Краснодар (491) ";
+                break;
+            case 800:
+                cityNames = "Киров (800) ";
+                break;
+            default:
+                throw new IllegalStateException("Упс:( Мы не знаем города с кодом: " + codeOfCity);
+        }
+        return cityNames;
+    }
+    public static Double priceForCall(int codeOfCity, int minForCall){
+
+        double finalCallPrice = 0;
+        if (codeOfCity == 905) {
+            finalCallPrice = minForCall * 4.15;
+        } else
+        if (codeOfCity == 194) {
+            finalCallPrice = minForCall * 1.98;
+        } else
+        if (codeOfCity == 491) {
+            finalCallPrice = minForCall * 2.69;
+        } else
+        if (codeOfCity == 800) {
+            finalCallPrice = minForCall * 5;
+        } else System.out.println("Вы ввели некорректный код города, перезапустите программу и попробуйте еще раз");
+        return (double) Math.round(finalCallPrice * 100) / 100;
+    }
+    public static String readerFromKeyboard() throws IOException {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String entered_value = reader.readLine();
-        return entered_value;
+        String enteredValue = reader.readLine();
+        return enteredValue;
     }
     public static Integer readIntFromKeyboard() throws IOException {
         boolean isNotCorrectInput = true;
         while (isNotCorrectInput) {
             try {
-                return Integer.valueOf(reader_from_keyboard());
+                return Integer.valueOf(readerFromKeyboard());
             } catch (NumberFormatException exception) {
                 System.out.println("Вы ввели не число");
             }
         }
         return 0;
     }
-    public static boolean positive_number( int num) {
-        boolean is_positive = false; // положительное
+    public static boolean positiveNumber(int num) {
+        boolean isPositive = false; // положительное
         if (num > 0) {
-            is_positive = true;
+            isPositive = true;
     }
-        return is_positive;
+        return isPositive;
     }
-
-    public static boolean negative_number( int num) {
-        boolean is_negative = false; // отрицательное
+    public static String positiveOrNegativeNumberStringDescription(int num) {
+        String positiveNumberStringDescription;
+        if (positiveNumber(num)){
+            positiveNumberStringDescription = "положительное ";
+        } else positiveNumberStringDescription = "отрицательное ";
+        return positiveNumberStringDescription;
+    }
+    public static boolean negativeNumber(int num) {
+        boolean isNegative = false; // отрицательное
         if (num < 0) {
-            is_negative = true;
+            isNegative = true;
         }
-        return is_negative;
-    }
-    public static boolean odd_number( int num){ // нечетное
-        boolean is_odd = false; // нечетное
-        if (num%2==1) {
-            is_odd = true;
-        }
-        return is_odd;
-    }
-    public static boolean even_number( int num) {// четное
-        boolean is_even = false; // нечетное
-        if (num%2==0) {
-            is_even = true;
-        }
-        return is_even;
+        return isNegative;
     }
 
+    public static boolean isEvenNumber(int num) {// четное
+        boolean isEven = false; // нечетное
+        if (num%2==0) {
+            isEven = true;
+        }
+        return isEven;
+    }
+//    public static boolean isOddNumber(int num) {// нечетное
+//        boolean isOdd = false; // четное
+//        if (num%2==0) {
+//            isOdd = true;
+//        }
+//        return isOdd;
+//    }
+    public static String evenOrOddNumberStringDescription(int num) {// четное
+        String evenOrOddNumberStringDescription;
+        if (isEvenNumber(num)){
+            evenOrOddNumberStringDescription = "четное ";
+        } else evenOrOddNumberStringDescription = "нечетное ";
+        return evenOrOddNumberStringDescription;
+    }
     }
